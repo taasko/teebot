2011fce640bb418c5ed85169dbd5d04d757409f7
Taavi S <taavi.skog@city.fi>
2020-04-15 15:01:06 +0300

    Better Teeworlds econ. Better username handling.

--- index.js
+++ index.js
@@ -1,34 +1,28 @@
 require("dotenv").config();
 const Discord = require("discord.js");
-const TeeworldsEcon = require("@w3groupfinland/teeworlds-econ");
-
-const bot = new Discord.Client({ token: process.env.DISCORD_TOKEN });
-const econ = new TeeworldsEcon(
-  process.env.TEEWORLDS_HOST,
-  process.env.TEEWORLDS_PORT,
-  process.env.TEEWORLDS_SECRET
-);
+const TeeworldsEcon = require("teeworlds-econ-api/build/TwEconClient");
 
 /*
  * Initialize connections.
  */
 
+const bot = new Discord.Client({ token: process.env.DISCORD_TOKEN });
 bot.login();
 bot.on("ready", () => console.log("Discord bot connected."));
 
+const econ = new TeeworldsEcon.TwEconClient(
+  process.env.TEEWORLDS_HOST,
+  process.env.TEEWORLDS_PORT,
+  process.env.TEEWORLDS_SECRET
+);
 econ.connect();
-econ.on("online", () => console.log("Teeworlds econ connected."));
 
 /*
  * Listen for Teeworlds events.
  */
 
-econ.on("enter", (e) => {
-  changeTeam(e.player, e.team);
-});
-
-econ.on("team_join", (e) => {
-  changeTeam(e.player, e.team);
+econ.on("game.team_join", (e) => {
+  changeTeam(e.clientName, e.teamId);
 });
 
 /*
@@ -40,16 +34,20 @@ const CTF_TEAM_MAPPING = { 0: "RED", 1: "BLUE" };
 function changeTeam(playerName, teamId) {
   bot.guilds.cache.forEach((guild) => {
     const members = guild.members.cache;
-    const member = members.find((member) => member.nickname === playerName);
+    const member = members.find(
+      (m) =>
+        (m.nickname || m.user.username).toLowerCase() ===
+        playerName.toLowerCase()
+    );
 
     if (member) {
       const channels = guild.channels.cache;
-      const teamChannelId = channels.find((channel) =>
+      const channel = channels.find((channel) =>
         channel.name.includes(CTF_TEAM_MAPPING[teamId])
-      ).id;
+      );
 
-      if (teamChannelId) {
-        member.voice.setChannel(teamChannelId);
+      if (channel && channel.id) {
+        member.voice.setChannel(channel.id);
       }
     }
   });
